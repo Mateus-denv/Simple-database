a68d2f5665e3b38e913a89d9e5483be5bd6d1a4d
Mateus De Jesus Santos <mateussant0sc0osta@outlook.com>
2025-05-15 19:51:53 -0300

    Renomeado/Renamed
    Nome do arquivo não estava correspondente.

--- main.py
+++
@@ -1,7 +0,0 @@
-while True:
-    print(">>>>>>>  INTERLEG   <<<<<<<")
-    print("\n1º)CADASTRO DE EMPRESAS \
-        \n2º)LANÇAMENTOS DE PRODUTOS \
-        \n3º)CONTROLE DE ESTOQUE"
-    )
-    break
