b3e2fd25aa75a2ae0921cbdb9823d04a7c22c0b0
Mateus De Jesus Santos <mateussant0sc0osta@outlook.com>
2025-05-22 09:56:31 -0300

    indentação

--- menu.py
+++ menu.py
@@ -1,24 +1,27 @@
 
 def fun_menu(autorização):
     print(">>>>>>>  INTERLEG   <<<<<<<")
-    while True:
-        escolha = input("\
-            \n1º)CADASTRO DE EMPRESAS \
-            \n2º)LANÇAMENTOS DE PRODUTOS \
-            \n3º)CONTROLE DE ESTOQUE\n|>>"
-        )
-        if escolha == "1":
-            print("entrou 1")
-            import CompanyRegistration
-            CompanyRegistration.CompanyRegistration()
-        elif escolha == "2":
-            print("entrou 2")
-        elif escolha == "3":
-            print("entrou 3")
-        else:
-            print(" Opcão inexistente!!! ")
-            continue
-        break
+    if autorização == 1:
+        while True:
+            escolha = input("\
+                \n1º)CADASTRO DE EMPRESAS \
+                \n2º)LANÇAMENTOS DE PRODUTOS \
+                \n3º)CONTROLE DE ESTOQUE\n|>>"
+            )
+            if escolha == "1":
+                print("entrou 1")
+                import CompanyRegistration
+                CompanyRegistration.fun_main(1)
+            elif escolha == "2":
+                print("entrou 2")
+            elif escolha == "3":
+                print("entrou 3")
+            else:
+                print(" Opcão inexistente!!! ")
+                continue
+            break
+    else:
+        print("Não autorizado")
 
 
 
